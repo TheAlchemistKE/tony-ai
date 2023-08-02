cfd000c76247f2fbbf9fbc50c985162e552dc6c8
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2023-08-02 19:26:28 +0300

    add route awareness

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,8 +1,10 @@
+"use client"
 import { cn } from '@/lib/utils'
 import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon } from 'lucide-react'
 import { Montserrat } from 'next/font/google'
 import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 const montserrat = Montserrat({
@@ -30,6 +32,12 @@ const routes = [
         color: "text-pink-700",
     },
     {
+        label: "Video Generation",
+        icon: VideoIcon,
+        href: '/video',
+        color: "text-orange-700",
+    },
+    {
         label: "Music Generation",
         icon: Music,
         href: '/music',
@@ -48,6 +56,7 @@ const routes = [
     },
 ]
 export default function Sidebar() {
+    const pathname = usePathname()
   return (
     <div className='space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white'>
         <div className='px-3 py-2 flex-1'>
@@ -66,7 +75,8 @@ export default function Sidebar() {
                     <Link 
                     href={route.href}
                     key={route.href}
-                    className='text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition'>
+                    className={cn('text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition', 
+                    pathname === route.href ? 'text-white bg-white/10' : 'text-zinc-400')}>
                         <div className='flex items-center flex-1'>
                             <route.icon className={cn('h-5 w-5 mr-3', route.color)}/>
                             {route.label}
