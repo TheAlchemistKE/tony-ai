6e7eac677313b36f2fd180bb874f2e4ebc9e4b4e
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2023-08-02 19:24:42 +0300

    fix component hydation issue

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 import {Button} from '@/components/ui/button'
 import { Menu } from 'lucide-react'
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
 import Sidebar from '@/components/sidebar'
 
 export default function MobileSidebar() {
+  const [isMounted, setIsMounted] = useState(false)
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  if (!isMounted) {
+    return null
+  }
+  
   return (
     <Sheet>
         <SheetTrigger>
